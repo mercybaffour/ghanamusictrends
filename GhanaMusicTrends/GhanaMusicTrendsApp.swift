071ba1883a48aefd156e1a729a50c58ec1f48814
Mercy Baffour <mercy.baffour@gmail.com>
2023-02-11 14:39:11 -0500

    Initial Commit

---
+++ GhanaMusicTrends/GhanaMusicTrendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhanaMusicTrendsApp.swift
+//  GhanaMusicTrends
+//
+//  Created by David Baffour on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GhanaMusicTrendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
